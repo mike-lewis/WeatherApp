a71ed8d5cc912d02190ab292ac9f8eb48ab1cb28
Mike <mike@Mikes-Mac-mini.local>
2016-02-15 23:11:21 -0800

    First commit

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -10,8 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cityNameTextField: UITextView!
+    @IBOutlet weak var cityTempLabel: UILabel!
+    @IBOutlet weak var cityNameLabel: UILabel!
+    
+    @IBAction func getDataButton(sender: AnyObject) {
+        
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        getWeatherData("http://api.openweathermap.org/data/2.5/weather?q=Vancouver&APPID=a14927833ec1779e7ef2634dd07ae47e")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +29,42 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func getWeatherData(urlString: String) {
+        let url = NSURL(string: urlString)
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url!) { (data, responce, error) in
+            dispatch_async(dispatch_get_main_queue(), {
+                self.setLabels(data!)
+            })
+        }
+        print("before resume")
+        task.resume()
+        
+    }
+    
+    func setLabels(weatherData: NSData) {
+
+        do {
+            let json = try NSJSONSerialization.JSONObjectWithData(weatherData, options: []) as! NSDictionary
+            if let name = json["name"] as? String {
+                cityNameLabel.text = name
+                print(name)
+            }
+
+            if let main = json["main"] as? NSDictionary {
+                if let temp = main["temp"] as? Double {
+                    print(temp)
+                    cityTempLabel.text = String(format: "%.1f", temp)
+
+                }
+            }
+            print("almost end")
+        } catch {
+            //write error handling code here
+            print("catch block")
+        }
+        print("end")
+  }
 
 }
 
